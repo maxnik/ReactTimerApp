09087227e1407ce631a1e1532b310c882406bcd4
Max Nikolenko <maxim.nikolenko@gmail.com>
2017-09-10 23:24:34 +0300

    Updates status to stop when count reaches zero

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -37,12 +37,21 @@ export default class Countdown extends React.Component {
 		}
 	}
 
+	componentWillUnmount() {
+		clearInterval(this.timer);
+		this.timer = undefined;
+	}
+
 	startTimer() {
 		this.timer = setInterval(() => {
 			const newCount = this.state.count - 1;
 			this.setState({
 				count: newCount >= 0 ? newCount : 0
 			});
+
+			if (newCount === 0) {
+				this.setState({countdownStatus: 'stopped'});
+			}
 		}, 1000);
 	}
 
